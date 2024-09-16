40a7bcdbf00ab7f3e01b21e04138e3f25af627ed
Yarri4 <ddrreennaall@mail.ru>
2024-09-16 11:00:03 +0300

    Added module_1_4

--- module_1_4/module_1_4.py
+++ module_1_4/module_1_4.py
@@ -1,2 +1,20 @@
-my_string = input('Python: ')
-print('ПРивет,', my_string)
+# my_string = input('Введи что то: ')
+# year = 2024
+# birth = input('Год рождения? ')
+# age = year - int(birth)
+# print('Привет,', my_string)
+# print('Вам ',age, 'лет' )
+
+
+
+
+# 2)
+my_string = input('что то: ')
+print(len(my_string))
+
+# 3)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
