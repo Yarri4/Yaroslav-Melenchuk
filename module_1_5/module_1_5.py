27eabc56d5b764a2c858eaec0b6caa1fec92e738
Yarri4 <ddrreennaall@mail.ru>
2024-09-16 13:25:16 +0300

    Added module_1_5, module_1_6

---
+++ module_1_5/module_1_5.py
@@ -0,0 +1,14 @@
+immutable_var = tuple_= 1,'Москва',3,4
+print(immutable_var[1])
+
+
+#3) immutable_var[1]=2 #TypeError: 'tuple' object does not support item assignment. Кортеж не поддерживает обращение по элементам.
+
+print('Цель 2):',immutable_var)
+
+
+mutable_list = list_ = [1,'Москва',3,4]
+mutable_list[1]=2
+
+print('Цель 4):', mutable_list)
+
---
+++ module_1_6/module_1_6.py
@@ -0,0 +1,21 @@
+my_dict  = {'Tom': 1997 , 'Sam': 1990}
+
+
+
+print('Цель 2):',my_dict)
+my_dict['Kris'] = 1979
+print('Цель 2):',my_dict['Tom'])
+
+
+my_dict['Den']=1991
+my_dict['Pit']=1989
+print('Цель 2):',my_dict)
+
+
+
+my_set = set = {1,2,3,4,5,1,2,3,4,5}
+my_set.add(6)
+my_set.add(7)
+print('Цель 3):',my_set)
+my_set.discard(7)
+print('Цель 3):',my_set)
