ffec43a16a07bdded7132f788e4fb8e1846eb6d9
Yarri4 <ddrreennaall@mail.ru>
2024-09-18 15:45:00 +0300

    Выполнено задание 2 модуля 2

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+# 2023/09/29 00:00|Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else."
+
+first = input('Введите первое число: ')
+second = input('Введите второе число: ')
+third = input('Введите третье число: ')
+
+if first == second and second == third and third == first:
+    print(3)
+elif first == second or second == third or third == first:
+    print(2)
+else:
+    print(0)
+    
+    
+    
