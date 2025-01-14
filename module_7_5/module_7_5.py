dcff8c30c1eb0a4ef90a14799c53581dd26795e9
Yarri4 <ddrreennaall@mail.ru>
2025-01-14 13:33:16 +0300

    Выполнено 7_5

--- module_7_5/module_7_5.py
+++ module_7_5/module_7_5.py
@@ -3,12 +3,12 @@
 import os, time
 from os.path import join, getmtime, getsize, dirname
 directory = "."     # тестировать будем в папке проекта
-for root, dirs, files in os.walk(directory):    # 1
+for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(root, file)     # 2
-        filetime = os.path.getmtime(file)       # 3
+        filepath = os.path.join(root, file)
+        filetime = os.path.getmtime(file)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(file)        # 4
+        filesize = os.path.getsize(file)
         # parent_dir = os.path.dirname(os.path.abspath(file)) # полный путь
-        parent_dir = os.path.dirname(directory)  # относительный путь # 5
+        parent_dir = os.path.dirname(directory)  # относительный путь
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
