35517e46d002211b70f5bff3a4d6f75d79b1a202
Yarri4 <ddrreennaall@mail.ru>
2025-01-20 11:26:52 +0300

    Выполнено 2_dop

---
+++ module_10_3/module_10_3.py
@@ -0,0 +1,45 @@
+# 2023/12/11 00:00|Домашнее задание по теме "Блокировки и обработка ошибок"
+
+import threading
+import random
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for _ in range(100):
+
+            amount = random.randint(50, 500)
+            print(f"Запрос на пополнение:{amount}")
+            self.balance += amount
+            print(f"Пополнение: {amount}. Баланс: {self.balance}")
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            time.sleep(0.001)
+
+    def take(self):
+        for _ in range(100):
+            amount = random.randint(50, 500)
+            print(f"Запрос на снятие:{amount}")
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f"Снятие: {amount}. Баланс: {self.balance}")
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+            time.sleep(0.001)
+
+bank = Bank()
+deposit_thread = threading.Thread(target=bank.deposit)
+take_thread = threading.Thread(target=bank.take)
+
+deposit_thread.start()
+take_thread.start()
+
+deposit_thread.join()
+take_thread.join()
+
+print(f"Итоговый баланс: {bank.balance}")
